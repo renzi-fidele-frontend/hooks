803b6ea579cee3ccae1ebf0f141badcab68d8ffa
Renzi Fidele <renzifidele2001@gmail.com>
2023-11-09 23:22:53 +0200

    Estruturando melhor

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,22 @@
 import { createContext, useEffect, useReducer, useState } from "react";
 import "./App.css";
 import HookUseReducer from "./EstudandoHooks/HookUseReducer";
+import HookUseCallback from "./EstudandoHooks/HookUseCallback";
 
 export const ThemeContext = createContext();
 
-function App() {
-   const ThemeReducer = (state, action) => {
-      switch (action.type) {
-         case "Light":
-            return { theme: "white", color: "black" };
-         case "Dark":
-            return { theme: "black", color: "white" };
-         default:
-            return state;
-      }
-   };
+const ThemeReducer = (state, action) => {
+   switch (action.type) {
+      case "Light":
+         return { theme: "white", color: "black" };
+      case "Dark":
+         return { theme: "black", color: "white" };
+      default:
+         return state;
+   }
+};
 
+function App() {
    const [theme, themeDispatch] = useReducer(ThemeReducer, {
       theme: "white",
       color: "black",
@@ -26,6 +27,9 @@ function App() {
          <ThemeContext.Provider value={{ theme, themeDispatch }}>
             {/*Use Reducer*/}
             <HookUseReducer />
+
+            {/*UseCallback */}
+            <HookUseCallback />
          </ThemeContext.Provider>
       </div>
    );
---
+++ src/EstudandoHooks/HookUseCallback.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const HookUseCallback = () => {
+  return (
+    <div>HookUseCallback</div>
+  )
+}
+
+export default HookUseCallback
